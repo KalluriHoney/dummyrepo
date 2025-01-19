10e2bc300463735ca634406bb7e601285aa984a2
KalluriHoney <kallurisrinu.web@gmail.com>
2025-01-19 12:52:25 +0530

    Home changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const productRoutes = require('./routes/productRoutes');
 
  
 const app = express();
-const PORT = 4000; 
+const PORT = process.env.PORT || 4000; 
 
  
 dotEnv.config();
@@ -29,7 +29,7 @@ app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`); 
 })
 
-app.use('/home', (req, res) => {
+app.use('/', (req, res) => {
     res.send("Welcome to Restaurent") 
 })
 
